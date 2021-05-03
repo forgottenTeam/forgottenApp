ce92ffa57b34418e4d544be48cf7656c7265ce3e
Jedrzej <ohma_tehus@wp.pl>
2021-05-03 20:20:58 +0200

    Dodanie nowej listy po kategorii

--- app/src/main/java/com/example/urbexexploration/places/PlaceListViewModel.java
+++ app/src/main/java/com/example/urbexexploration/places/PlaceListViewModel.java
@@ -1,15 +1,19 @@
 package com.example.urbexexploration.places;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.urbexexploration.data.Place;
 import com.example.urbexexploration.data.PlacesRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlaceListViewModel extends ViewModel {
     private PlacesRepository repository;
+    private MutableLiveData<List<Place>> filteredDataList;
+
 
     public PlaceListViewModel () {
         repository = new PlacesRepository();
@@ -18,5 +22,18 @@ public class PlaceListViewModel extends ViewModel {
 
     public LiveData<List<Place>> getPlacesLiveData() {
         return repository.getPlacesLiveData();
+    }
+
+    public void filterList (String category) {
+        List<Place> list = repository.getPlacesLiveData().getValue();
+        if (list != null) {
+            List<Place> listCategory = new ArrayList<>();
+            for (Place p : list) {
+                if (p.getCategory().equalsIgnoreCase(category)) {
+                    listCategory.add(p);
+                }
+            }
+            filteredDataList.setValue(listCategory);
+        }
     }
 }
