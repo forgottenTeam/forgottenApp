26a671d4dfbd41f29573ec79442778144b277872
jedrzejMan <ohma_tehus@wp.pl>
2021-06-06 21:22:59 +0200

    testy dla klasy Place

---
+++ app/src/test/java/com/example/urbexexploration/data/PlaceUnitTest.java
@@ -0,0 +1,30 @@
+package com.example.urbexexploration.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Testy dla klasy Place
+ */
+public class PlaceUnitTest {
+
+    @Test
+    public void ifIdIsPresent_returnPhotoUriWithId() {
+        int testId = 28;
+        Place newPlaceTest = new Place (testId, "", "", "", "", "", 0.0, 0.0, "");
+        assertEquals(newPlaceTest.getPhotoUri(), Place.BASE_IMAGE_URL + testId);
+    }
+
+    @Test
+    public void ifPhotoUriPresent_returnPhotoUriNotNull() {
+        Place newPlaceTest = new Place (0, "", "", "", "", "", 0.0, 0.0, "testPhotoUri");
+        assertNotNull(newPlaceTest.getPhotoUri());
+    }
+
+    @Test
+    public void ifPhotoUriNotPresent_returnPhotoUriIsNull() {
+        Place newPlaceTest = new Place (0, "", "", "", "", "", 0.0, 0.0, null);
+        assertNull(newPlaceTest.getPhotoUri());
+    }
+}
